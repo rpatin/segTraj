9fdc0f6f1e3f375932e03c567266b6b0a275ed50
Marie-Pierre Etienne <marie.etienne@agroparistech.fr>
2017-01-25 16:44:21 +0100

    Minor change of notation x --> z

--- R/Gmean_simultanee.R
+++ R/Gmean_simultanee.R
@@ -4,7 +4,7 @@
 #' @param lmin minimum size for a segment, default value is 2
 #' @return the cost matrix G(i,j) which contains the variance of the data between point (i+1) to point j
 #' @export
-Gmean_simultanee<-function(Don,lmin=2)
+Gmean_simultanee<-function(Don,lmin)
 {
   n = dim(Don)[2] 
 
@@ -12,21 +12,21 @@ Gmean_simultanee<-function(Don,lmin=2)
   ## every element of the list is the cost motrix for one signal
   matD_list=lapply(1:2,function(nb) {
     Res=matrix(Inf,n,n)
-    x=Don[nb,] #depend de la forme des donnees    
-    x2=x^2
-    x2i=cumsum(x2)
-    xi=cumsum(x)
-    x2i=x2i[lmin:n]
-    xi=xi[lmin:n]
-    Res[1,lmin:n]=x2i-((xi^2)/(lmin:n))
+    z=Don[nb,] #depend de la forme des donnees    
+    z2=z^2
+    z2i=cumsum(z2)
+    zi=cumsum(z)
+    z2i=z2i[lmin:n]
+    zi=zi[lmin:n]
+    Res[1,lmin:n]=z2i-((zi^2)/(lmin:n))
     nl=n-lmin+1
     for (i in 2:nl)
     {
       ni=n-i-lmin+3
-      x2i=x2i[2:ni]-x2[i-1] 
-      xi=xi[2:ni]-x[i-1]
+      z2i=z2i[2:ni]-z2[i-1] 
+      zi=zi[2:ni]-z[i-1]
       deno<-((i+lmin-1):n)-i+1
-      Res[i,(i+lmin-1):n]=x2i-((xi^2)/deno)
+      Res[i,(i+lmin-1):n]=z2i-((zi^2)/deno)
     }
     return(Res)
   })
@@ -34,3 +34,4 @@ Gmean_simultanee<-function(Don,lmin=2)
   matD=Reduce("+",matD_list)
   invisible(matD)
 }
+## debug note : lmin is correctly taken into account
