6b1472f4ecb957b08e08846c938b8dc13d117e2e
Marie-Pierre Etienne <marie.etienne@agroparistech.fr>
2017-01-25 16:40:53 +0100

    Adding an option to bisig_plot to represent the clustering segment

--- R/plot_simultanee.R
+++ R/plot_simultanee.R
@@ -2,11 +2,13 @@
 #' bisig_plot draws the plots of the bivaraite signal on the same plot (scale free)
 #' @param x the signal to be plotted
 #' @param rupt optionnal, if given add vertical lines at change points (rupt should a vector)
+#' @param mu optionnal the mean of each class of segment, 
+#' @param pop optionnal the cluster to whom each segment belongs to,
 #' 
 #' @export
 #' @return no value
 
-bisig_plot<- function(x, rupt=NULL, mu=NULL, pop=NULL){
+bisig_plot<- function(x, rupt=NULL, mu=NULL, pop=NULL, merge.seg=FALSE){
   n <- dim(x)[2]
   if(!is.null(rupt)){
     if(!is.matrix(rupt))
@@ -20,17 +22,31 @@ bisig_plot<- function(x, rupt=NULL, mu=NULL, pop=NULL){
   x.norm <- sweep(x = x.norm, MARGIN = 1, STATS = s, FUN = '/')
   plot(1:n,x.norm[1,], pch=19, col=2, ylab='', xlab = '', xaxt='n', yaxt='n')
   points(1:n, x.norm[2,], pch=19, col=3)
-  if(!is.null(rupt)){
+  if(!is.null(rupt)&!(merge.seg)){
     invisible( lapply(1:K, function(d){ abline(v=rupt[d,1], lwd=1.5)}))
+  }
+  if(!is.null(rupt)&(merge.seg)){
+    change <- which(diff(pop)!=0)
+    p <- length(change)
+    ruptProv <- matrix(NA, nrow=p+1, ncol=2)
+    ruptProv[1, 1] <- rupt[1,1]
+    ruptProv[1, 2] <- rupt[change[1],2]
+    ruptProv[p+1, 1] <- rupt[change[p]+1,1]
+    ruptProv[p+1, 2] <- rupt[K,2]
+    
+    if(p>1){
+      ruptProv[2:p,] <- sapply(1:(p-1), function(p_){c(rupt[change[p_]+1,1], 
+                                                       rupt[change[p_+1],2])})
+    }
+    invisible( lapply(1:(p+1), function(d){ abline(v=ruptProv[d,1], lwd=1.5)}))
+    
   }
   if(!is.null(mu) & !is.null(pop)){
     invisible( lapply(1:K, function(d){ 
-      lines(rupt[d,], (rep(mu[1,pop[d]],2)-m[1])/s[1], col=2)
-      lines(rupt[d,], (rep(mu[2,pop[d]],2)-m[2])/s[2], col=3)
+      lines(c(rupt[d,1],rupt[d,2]+1), (rep(mu[1,pop[d]],2)-m[1])/s[1], col=2)
+      lines(c(rupt[d,1],rupt[d,2]+1), (rep(mu[2,pop[d]],2)-m[2])/s[2], col=3)
     }))
   }
-  
-  
 }
 
 
